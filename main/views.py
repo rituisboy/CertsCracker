832a094b5b461761623fda56df2dc81becbe1937
rituisboy <riturajthakur100@gmail.com>
2023-12-20 16:13:17 +0530

    post method added

--- main/views.py
+++ main/views.py
@@ -3,11 +3,18 @@ from .serializers import QuestionsSerializer
 from .models import *
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 
-@api_view(["GET"])
+@api_view(["GET","POST"])
 def index(request):
-    question = Questions.objects.all()
-    serilaizer = QuestionsSerializer(question, many=True)
+    if request.method == "GET":
+        question = Questions.objects.all()
+        serilaizer = QuestionsSerializer(question, many=True)
 
-    return Response(serilaizer.data)
+        return Response(serilaizer.data)
+    if request.method == "POST":
+        serilaizer = QuestionsSerializer(data=request.data)
+        if serilaizer.is_valid ():
+            serilaizer.save()
+        return Response(serilaizer.data,status=status.HTTP_201_CREATED)
