e674c1165999eca8d5f5f7abe2bc8438426d9220
rituisboy <riturajthakur100@gmail.com>
2023-12-28 19:49:31 +0530

    delete added

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('api/',index,name='index')
+    path('api/',index,name='index'),
+    path('api/<int:id>',delete_question,name='delete')
 ]
--- main/views.py
+++ main/views.py
@@ -7,7 +7,7 @@ from rest_framework import status
 
 
 @api_view(["GET","POST"])
-def index(request):
+def index(request,id=None):
     if request.method == "GET":
         question = Questions.objects.all()
         serilaizer = QuestionsSerializer(question, many=True)
@@ -18,3 +18,12 @@ def index(request):
         if serilaizer.is_valid ():
             serilaizer.save()
         return Response(serilaizer.data,status=status.HTTP_201_CREATED)
+@api_view(['DELETE'])
+def delete_question(request, id):
+    try:
+        question = Questions.objects.get(id=id)
+    except Questions.DoesNotExist:
+        return Response({'error': 'Question not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    question.delete()
+    return Response({'message': 'Question deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
