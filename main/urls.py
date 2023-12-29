97eac6c6b3c99d15cfc54e2bfbecd9a219156657
rituisboy <riturajthakur100@gmail.com>
2023-12-29 17:38:12 +0530

    update added

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('api/',index,name='index'),
-    path('api/<int:id>',delete_question,name='delete')
+    path('api/<int:id>',delete_question,name='delete'),
+    path('api/update/<int:id>',update_question,name='update')
 ]
--- main/views.py
+++ main/views.py
@@ -6,24 +6,43 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-@api_view(["GET","POST"])
-def index(request,id=None):
+@api_view(["GET", "POST"])
+def index(request):
     if request.method == "GET":
         question = Questions.objects.all()
         serilaizer = QuestionsSerializer(question, many=True)
 
-        return Response(serilaizer.data)
+        return Response(serilaizer.data, status=status.HTTP_201_CREATED)
     if request.method == "POST":
         serilaizer = QuestionsSerializer(data=request.data)
-        if serilaizer.is_valid ():
+        if serilaizer.is_valid():
             serilaizer.save()
-        return Response(serilaizer.data,status=status.HTTP_201_CREATED)
-@api_view(['DELETE'])
+        return Response(serilaizer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(["DELETE"])
 def delete_question(request, id):
     try:
         question = Questions.objects.get(id=id)
     except Questions.DoesNotExist:
-        return Response({'error': 'Question not found'}, status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            {"error": "Question not found"}, status=status.HTTP_404_NOT_FOUND
+        )
 
     question.delete()
-    return Response({'message': 'Question deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+    return Response(
+        {"message": "Question deleted successfully"}, status=status.HTTP_204_NO_CONTENT
+    )
+
+
+@api_view(["PUT"])
+def update_question(request, id):
+    try:
+        question = Questions.objects.get(id=id)
+    except Questions.DoesNotExist:
+        return Response({"error": "no such question exist "})
+    serializer = QuestionsSerializer(question, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
